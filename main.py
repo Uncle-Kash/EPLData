551cd6446609b5d392ad14d0dc6db8181ab3cbc2
Kachi Awaji <webdev.awaji@gmail.com>
2022-05-11 19:08:04 +0100

    Add main code

---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+import requests
+
+
+standings_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
+
+data = requests.get(standings_url)
+
+soup = BeautifulSoup(data.text)
+
+standings_table = soup.select('table.stats_table')[0]
+
+links = standings_table.find_all('a')
+
+#select uses css selectors
+#find all only finds tags.
+#using list comprehension.
+#how does list comprehension work
+
+
+links = [l.get("href") for l in links]
+
+links = [l for l in links if '/squads/' in l]
+
+team_urls = [f"https://fbref.com{l}" for l in links] 
+
+print(team_urls)
+
+
+team_url = team_urls[0]
+
+data = requests.get(team_url)
+
+matches = pd.read_html(data.text, match="Scores & Fixtures")
+
+print(matches)
+print(matches[0])
